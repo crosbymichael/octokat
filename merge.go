e6c08869378b93de852959620062b3ec60a177cf
Michael Crosby <crosby.michael@gmail.com>
2013-11-01 12:26:37 -0700

    Fix merge path

--- merge.go
+++ merge.go
@@ -14,7 +14,7 @@ type Merge struct {
 //
 // See http://developer.github.com/v3/pulls/#merge-a-pull-request-merge-buttontrade
 func (c *Client) MergePullRequest(repo Repo, number string, options *Options) (merge Merge, err error) {
-	path := fmt.Sprintf("/repos/%s/pulls/%s/merge", repo, number)
+	path := fmt.Sprintf("repos/%s/pulls/%s/merge", repo, number)
 	err = c.jsonPut(path, options, &merge)
 	return
 }
